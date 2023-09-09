67389d5051f728556e40226669da34e65f2d6e84
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2023-09-09 23:09:19 +0800

    chore: 404 page

---
+++ src/pages/[...unknown].vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>unknown page</h2>
+</template>
--- types/typed-router.d.ts
+++ types/typed-router.d.ts
@@ -40,6 +40,7 @@ import type {
 declare module 'vue-router/auto/routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/[...unknown]': RouteRecordInfo<'/[...unknown]', '/:unknown(.*)', { unknown: ParamValue<true> }, { unknown: ParamValue<false> }>,
   }
 }
 
